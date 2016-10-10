4eb89f85a9072117ff0a0191ff5e3774c39b5d9e
rishikant42 <rksbtp@gmail.com>
2016-10-10 20:14:06 +0530

    solved problem no. 8

---
+++ P08.scm
@@ -0,0 +1,9 @@
+(define nil '())
+
+(define (remove x items)
+  (filter (lambda (y) (not (= x y))) items))
+
+(define (compress items)
+  (cond ((= (length items) 1) items)
+        ((= (car items) (cadr items)) (compress (cdr items)))
+        (else (cons (car items) (compress (cdr items))))))
