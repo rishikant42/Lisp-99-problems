c78c5294b686bdc2d9d18278fc11701322252b03
rishikant42 <rksbtp@gmail.com>
2016-10-13 08:29:27 +0530

    fixed code

--- P08.scm
+++ P08.scm
@@ -5,5 +5,5 @@
 
 (define (compress items)
   (cond ((= (length items) 1) items)
-        ((= (car items) (cadr items)) (compress (cdr items)))
+        ((eq? (car items) (cadr items)) (compress (cdr items)))
         (else (cons (car items) (compress (cdr items))))))
