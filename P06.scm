227575ef5053fadd634d4e076cebe51a431cd1f3
rishikant42 <rksbtp@gmail.com>
2016-10-13 08:28:40 +0530

    fixed code

--- P06.scm
+++ P06.scm
@@ -7,10 +7,8 @@
 
 (define (check list1 list2)
   (cond ((null? list1) true)
-        ((= (car list1) (car list2)) (check (cdr list1) (cdr list2)))
+        ((eq? (car list1) (car list2)) (check (cdr list1) (cdr list2)))
         (else false)))
 
 (define (palindrome? items)
   (check items (reverse items)))
-
-;; Items of list should be integer
