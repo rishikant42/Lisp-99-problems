bb48607901e1c72820f643bb989859222d46017c
rishikant42 <rksbtp@gmail.com>
2016-10-23 11:35:40 +0530

    added solution of P26

--- P26.scm
+++ P26.scm
@@ -1,3 +1,5 @@
+;; Generate the all combinations of K objects chosen from the N elements of a list ;;
+
 (define (split items n)
   (define (fn first rest count)
     (if (= count 0)
@@ -31,3 +33,22 @@
   (modify-flatten (map (lambda (x) 
                          (single-combine n (cons x (remove x items) ))) 
                        items)))
+
+;; 1 ]=> (combination 3 '(a b c d e))
+
+;;;Value 11: ((a b e) (a b d) (a b c) (b a e) (b a d) (b a c) (c a e) (c a d) (c a b) (d a e) (d a c) (d a b) (e a d) (e a c) (e a b))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;P26 solution;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;Generate the combinations of K distinct objects chosen from the N elements of a list;;;;;
+
+(define (combination1 k xs) 
+  (cond ((null? xs) '()) 
+        ((= k 1) (map list xs)) 
+        (else (append (map (lambda (x) (cons (car xs) x)) 
+                           (combination1 (- k 1) (cdr xs))) 
+                      (combination1 k (cdr xs))))))
+
+;; 1 ]=> (combination1 3 '(a b c d e))
+
+;; ;Value 12: ((a b c) (a b d) (a b e) (a c d) (a c e) (a d e) (b c d) (b c e) (b d e) (c d e))
